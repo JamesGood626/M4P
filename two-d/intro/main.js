e9e102bbeb10d6c010374f0d0794b00fc03461ec
JamesGood626 <jamesgood626@gmail.com>
2019-02-26 16:18:39 -0500

    Created a function to add a line... wooo

--- two-d/intro/main.js
+++ two-d/intro/main.js
@@ -1,19 +1,26 @@
 let app = new PIXI.Application({ width: 256, height: 256 });
 
-// Create the main stage for your display objects
-// var stage = new PIXI.Container();
+function newLine(app, xOrigin, yOrigin, xEnd, yEnd) {
+  // Create a new Graphics object and add it to the scene
+  const graphics = new PIXI.Graphics();
+  // Move it to the beginning of the line
+  graphics.position.set(xOrigin, yOrigin);
+  // Draw the line (endPoint should be relative to myGraph's position)
+  graphics
+    .lineStyle(4, 0xffffff)
+    .moveTo(0, 0)
+    .lineTo(xEnd, yEnd);
+  // Add the line to the stage
+  app.stage.addChild(graphics);
+}
 
-// Create a new Graphics object and add it to the scene
-let myGraph = new PIXI.Graphics();
-app.stage.addChild(myGraph);
+// newLine(app, 10, 10, 40, 40);
+// newLine(app, 100, 10, 40, 40);
 
-// Move it to the beginning of the line
-myGraph.position.set(50, 30);
-
-// Draw the line (endPoint should be relative to myGraph's position)
-myGraph
-  .lineStyle(4, 0xffffff)
-  .moveTo(0, 0)
-  .lineTo(40, 70);
+let i = 0;
+while (i < app._options.width) {
+  newLine(app, i * 20, i * 10, 40, 0);
+  i++;
+}
 
 document.body.append(app.view);
